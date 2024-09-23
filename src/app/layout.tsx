638507409c33be0267d5c9668780a7272fa93735
kennyAnyi9 <kennyanyi9@gmail.com>
2024-09-23 16:15:21 +0000

    update footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
 import { Gabarito } from "next/font/google";
-import "./globals.css";
-import { ShootingStars } from "@/components/ui/backgrounds/shooting-stars";
-import { StarsBackground } from "@/components/ui/backgrounds/shooting-background";
 import Footer from "./(landing)/Footer";
+import "./globals.css";
 
 const inter = Gabarito({ subsets: ["latin"] });
 
@@ -19,8 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}
-        <Footer/>
+      <body className={inter.className}>
+        {children}
+        <Footer />
       </body>
     </html>
   );
